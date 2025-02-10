319176d2a9edbad1721f6a4cb6c9e34e61d1fa04
leopiresjr <leopiresjunior@hotmail.com>
2025-02-10 17:53:59 -0300

    Atualizando app Streamlit

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
 
-st.title('Meu Aplicativo Streamlit')
-st.write('Este é um teste de deploy no Render!')
+st.header('Jogando uma moeda')
+
+number_of_trials = st.slider('Número de tentativas?', 1, 1000, 10)
+start_button = st.button('Executar')
+
+if start_button:
+    st.write(f'Executando o experimento de {number_of_trials} tentativas.')
+
+st.write('Ainda não é um aplicativo funcional. Em construção.')
